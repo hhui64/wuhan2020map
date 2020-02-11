26dc0f4b72f8ed153e384c18e9b20b5d11ea2701
Travis CI User <travis@example.org>
2020-02-11 23:50:23 +0000

    deploy

---
+++ precache-manifest.d0aa58011295cc8eb74c111d628ab147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff093fd9bc979096eeab",
+    "url": "/wuhan2020map/css/app.264a9312.css"
+  },
+  {
+    "revision": "6b6c4fce522ab3e9fa71",
+    "url": "/wuhan2020map/css/chunk-vendors.8600c2e0.css"
+  },
+  {
+    "revision": "9e2c64048150de3aeeecd0f63ca88039",
+    "url": "/wuhan2020map/index.html"
+  },
+  {
+    "revision": "ff093fd9bc979096eeab",
+    "url": "/wuhan2020map/js/app.9e85f97c.js"
+  },
+  {
+    "revision": "6b6c4fce522ab3e9fa71",
+    "url": "/wuhan2020map/js/chunk-vendors.49fa635b.js"
+  },
+  {
+    "revision": "5dd2bea26ca3a0720aba14e683ab06bb",
+    "url": "/wuhan2020map/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wuhan2020map/robots.txt"
+  }
+]);
