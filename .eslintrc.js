9be313af425ec72bbb293761c32a99ae43995e0a
hhui64 <907322015@qq.com>
2020-02-12 06:43:58 +0800

    chore: Move some babel configs to babel.config.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential"],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier": 0,
+    "no-unused-vars": 0,
+    "require-atomic-updates": 0,
+    "no-async-promise-executor": 0
   },
   globals: {
     "AMap": true
@@ -14,4 +18,4 @@ module.exports = {
   parserOptions: {
     parser: "babel-eslint"
   }
-};
+}
